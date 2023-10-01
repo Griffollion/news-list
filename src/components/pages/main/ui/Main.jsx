10a65a1c4f2d4968654d8ba9e64a484228afef5e
Denis Filippovich <frontend@ru.myfin.by>
2023-10-02 01:18:22 +0300

    added logo animation

--- src/components/pages/main/ui/Main.jsx
+++ src/components/pages/main/ui/Main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './Main.module.css'
 import { NavLink } from 'react-router-dom';
-import {ReactComponent as NalIcon} from 'images/icons/nal.svg'
 import {GLOBAL_BREAKPOINTS} from "components/shared/helpers/constants";
 import {Button} from "components/shared/Button";
 import useBreakpoint from "use-breakpoint";
+import Logo from "./components/Logo/Logo";
 
 const BREAKPOINTS = GLOBAL_BREAKPOINTS
 
@@ -16,9 +16,7 @@ const Main = () => {
                 <div className={styles['title']}>
                     NAL.BY
                 </div>
-                <div className={styles['logo']}>
-                    <NalIcon/>
-                </div>
+                <Logo/>
                 <div className={styles['btn-wrapper']}>
                     <NavLink to="/news-list">
                         <Button size={breakpoint === "mobile" ? '': "l"}>К списку сегодняшних новостей</Button>
